72102a22b06218b3dffdddfefa573fe6634b0a0f
TerXGamer <62888197+TerXGamer@users.noreply.github.com>
2026-06-11 15:18:56 +0300

    Use compressed app script in build check

--- build.mjs
+++ build.mjs
@@ -1,6 +1,6 @@
 import { access } from "node:fs/promises";
 
-const requiredFiles = ["index.html", "styles.css", "app.js", "server.mjs"];
+const requiredFiles = ["index.html", "styles.css", "app.js.br.b64", "server.mjs"];
 
 await Promise.all(requiredFiles.map((file) => access(file)));
 
